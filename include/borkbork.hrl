2296d46265420fc24ccf133748bc4ae80367ed6e
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2018-01-02 21:30:29 +0100

    Added a header file for the ast node definition.

---
+++ include/borkbork.hrl
@@ -0,0 +1,75 @@
+
+-record(drakon, {
+    name,
+    parameters,
+
+    diagram, % primitive | silhouette
+
+    attributes = #{}
+}).
+
+-record(primitive, {
+    skewer,
+
+    attributes = #{}
+}).
+
+-record(silhouette, {
+    branches = [],
+
+    attributes = #{}
+}).
+
+-record(skewer, {
+    list = [], % The sequence of items on the skewer.
+
+    attributes = #{}
+}).
+
+-record(branch, {
+    icon_content,
+    skewer,
+    address,
+
+    attributes = #{}
+}).
+
+-record(question, {
+    icon_content,
+
+    right_label,
+    right_skewer,
+    right_address,
+
+    attributes = #{}
+}).
+
+-record(action, {
+    icon_content,
+
+    attributes = #{}
+}).
+
+-record(insertion, {
+    icon_content,
+
+    attributes = #{}
+}).
+
+-record(address, {
+    icon_content,
+
+    attributes = #{}
+}).
+
+-record(link, {
+    identifier,
+
+    attributes = #{}
+}).
+
+-record(connector, {
+    identifier,
+
+    attributes = #{}
+}).
