b04c86d4179ea6fe2974c2ff342deb9695687005
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2018-01-07 13:26:28 +0100

    Added copyright notice

--- src/b_scanner.erl
+++ src/b_scanner.erl
@@ -1,6 +1,9 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at http://mozilla.org/MPL/2.0/.
 %%
-%%
-%%
+%% @author Maas-Maarten Zeeman
+%% @copyright (c) 2018 Maas-Maarten Zeeman <me@mmzeeman.nl>
 
 -module(b_scanner).
 
